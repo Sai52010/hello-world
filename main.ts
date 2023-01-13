28b23f0a3fc0f3afbc4c14848e7e5e8f3fa564a6
Sai52010 <122099418+Sai52010@users.noreply.github.com>
2023-01-13 12:16:36 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 input.onButtonPressed(Button.A, function () {
     music.playMelody("C5 B A G F E D C ", 500)
-    basic.showIcon(IconNames.Heart)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Butterfly)
