34e70b844184251c44b9b60ab8a1016ccee1ec20
Sai52010 <122099418+Sai52010@users.noreply.github.com>
2023-01-13 12:22:47 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index < 99; index++) {
+        music.ringTone(10000000000000000)
         music.playMelody("C5 C5 C5 C5 C5 C5 C5 C5 ", 500)
     }
 })
